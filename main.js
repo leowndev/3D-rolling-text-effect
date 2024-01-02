2aca0832f0eebce5bfba9811b4a0bd9f20bf9a31
Leonardo <leonardogt2308@gmail.com>
2024-01-02 01:20:52 -0300

    Animate opacity to show only 1 text at time

--- main.js
+++ main.js
@@ -7,4 +7,14 @@ const tl = gsap.timeline();
 texts.forEach(text => {
   const splitText = new SplitTextJS(text);
   console.log(splitText)
+
+  tl
+    // incoming animation
+    .from(splitText.chars, {
+      opacity: 0,
+    }, "<")
+    // outcoming animation
+    .to(splitText.chars, {
+      opacity: 0
+    }, "<1")
 })
